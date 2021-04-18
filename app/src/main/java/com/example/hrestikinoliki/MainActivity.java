4496095d4653a4a3500dc219be850d8445366160
bohdan093 <bohdan.inform@gmail.com>
2021-04-18 21:38:21 +0300

    third toast

--- app/src/main/java/com/example/hrestikinoliki/MainActivity.java
+++ app/src/main/java/com/example/hrestikinoliki/MainActivity.java
@@ -33,6 +33,7 @@ public static final String SIGN = "SIGN";
 
         Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();;
         Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();;
+        Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();;
 
 
     }
